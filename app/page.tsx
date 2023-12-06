1675415c041fe26c2dd20e4a5bdc35482e0c09e1
Mahmoud <ma.abdel.mohsen@gmail.com>
2023-12-06 12:08:03 +0100

    add sign in/ out buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
 import { Link } from "@nextui-org/link";
 import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
+import { Code } from "@nextui-org/code";
 import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle, titleUnderLine } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import { Card, Text, Metric, Flex, ProgressBar } from "@tremor/react";
-
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
-	return (
-		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-			<div className="inline-block max-w-lg text-center justify-center">
-			{/* <Card className="max-w-xs mx-auto mb-4">
+  return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <div className="inline-block max-w-lg text-center justify-center">
+        {/* <Card className="max-w-xs mx-auto mb-4">
     <Text>Sales</Text>
     <Metric>$ 71,465</Metric>
     <Flex className="mt-4">
@@ -21,49 +24,49 @@ export default function Home() {
     </Flex>
     <ProgressBar value={32} className="mt-2" />
   </Card> */}
-				<h1 className={title() } >Deliver&nbsp;</h1>
-				<h1 className={title({ color: "blue" })}>personalized&nbsp;</h1>
-				<br />
-				<h1 className={title({ color: "violet" })}>
-					educational&nbsp; 
-				</h1>
-				<h1 className={title()}>
-					content, and leave no one&nbsp;
-				</h1>
-				<h1 className={title({ color: "pink" })}>
-					behind
-				</h1>
-				<h2 className={subtitle({ class: "mt-4" })}>
-					Educational system for tech bootcamps
-				</h2>
-			</div>
+        <h1 className={title()}>Deliver&nbsp;</h1>
+        <h1 className={title({ color: "blue" })}>personalized&nbsp;</h1>
+        <br />
+        <h1 className={title({ color: "violet" })}>educational&nbsp;</h1>
+        <h1 className={title()}>content, and leave no one&nbsp;</h1>
+        <h1 className={title({ color: "pink" })}>behind</h1>
+        <h2 className={subtitle({ class: "mt-4" })}>
+          Educational system for tech bootcamps
+        </h2>
+      </div>
 
-			<div className="flex gap-3">
-				<Link
-					isExternal
-					href={siteConfig.links.docs}
-					className={buttonStyles({ color: "primary", radius: "full", variant: "shadow" })}
-				>
-					Documentation
-				</Link>
-				<Link
-					isExternal
-					className={buttonStyles({ variant: "bordered", radius: "full" })}
-					href={siteConfig.links.github}
-				>
-					<GithubIcon size={20} />
-					GitHub
-				</Link>
-			</div>
+      <div className="flex gap-3">
+        <Link
+          isExternal
+          href={siteConfig.links.docs}
+          className={buttonStyles({
+            color: "primary",
+            radius: "full",
+            variant: "shadow",
+          })}
+        >
+          Documentation
+        </Link>
+        <LoginLink
+          className={buttonStyles({
+            color: "default",
+            radius: "full",
+            variant: "ghost",
+          })}
+        >
+          {" "}
+          Sign in{" "}
+        </LoginLink>
+      </div>
 
-			<div className="mt-8">
-				<Snippet  variant="bordered" size="sm">
-				npm install open-tech-camp
-					{/* <span>
+      <div className="mt-8">
+        <Snippet variant="bordered" size="sm">
+          npm install open-tech-camp
+          {/* <span>
 						Get started by forking <Code color="primary">GitHub repo</Code>
 					</span> */}
-				</Snippet>
-			</div>
-		</section>
-	);
+        </Snippet>
+      </div>
+    </section>
+  );
 }
--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,14 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Open Tech Camp",
-	description: "Deliver personalized educational content, and leave no one behind.",
-	navItems: [
-		{
-			label: "Home",
-			href: "/",
-		},
+  name: "Open Tech Camp",
+  description:
+    "Deliver personalized educational content, and leave no one behind.",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
     {
       label: "Docs",
       href: "/docs",
@@ -23,47 +24,47 @@ export const siteConfig = {
     {
       label: "About",
       href: "/about",
-    }
-	],
-	navMenuItems: [
-		{
-			label: "Profile",
-			href: "/profile",
-		},
-		{
-			label: "Dashboard",
-			href: "/dashboard",
-		},
-		{
-			label: "Projects",
-			href: "/projects",
-		},
-		{
-			label: "Team",
-			href: "/team",
-		},
-		{
-			label: "Calendar",
-			href: "/calendar",
-		},
-		{
-			label: "Settings",
-			href: "/settings",
-		},
-		{
-			label: "Help & Feedback",
-			href: "/help-feedback",
-		},
-		{
-			label: "Logout",
-			href: "/logout",
-		},
-	],
-	links: {
-		github: "",
-		twitter: "",
-		docs: "",
-		discord: "",
-    sponsor: ""
-	},
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Profile",
+      href: "/profile",
+    },
+    {
+      label: "Dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "Projects",
+      href: "/projects",
+    },
+    {
+      label: "Team",
+      href: "/team",
+    },
+    {
+      label: "Calendar",
+      href: "/calendar",
+    },
+    {
+      label: "Settings",
+      href: "/settings",
+    },
+    {
+      label: "Help & Feedback",
+      href: "/help-feedback",
+    },
+    {
+      label: "Logout",
+      href: "/logout",
+    },
+  ],
+  links: {
+    github: "",
+    twitter: "",
+    docs: "",
+    discord: "",
+    sponsor: "",
+  },
 };
