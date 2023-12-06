abe9d1be4b7fa6d44e96aca4f563de9c66c7a735
Mahmoud <ma.abdel.mohsen@gmail.com>
2023-12-06 10:19:42 +0100

    remove search bar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -93,7 +93,7 @@ export const Navbar = () => {
 					</Link>
 					<ThemeSwitch />
 				</NavbarItem>
-				<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+				{/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
 				<NavbarItem className="hidden md:flex">
 
 				</NavbarItem>
