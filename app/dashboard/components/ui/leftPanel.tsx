1c4c0aedff6de5083959a4531754cfe8ef6a7141
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-12-08 21:28:27 +0100

    finish right and left panels

--- app/dashboard/components/ui/leftPanel.tsx
+++ app/dashboard/components/ui/leftPanel.tsx
@@ -6,25 +6,109 @@ import {
   Link,
   Card,
   CardBody,
+  User,Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, getKeyValue
 } from "@nextui-org/react";
 
 export default function LeftPanel() {
   const defaultContent =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
-  const myAwards = (
-    <ol className="list-disc">
-      <li>Top performer week 4 </li>
-      <Link href="https://nextui.org"> iam a link</Link>
-      <br />
-      <Card isHoverable isPressable>
-        <CardBody>
-          <p>Make beautiful websites regardless of your design experience.</p>
-        </CardBody>
-      </Card>
-      <Button> iam a button</Button>
-      <li>Top performer week 4 </li>
-    </ol>
+//-----------------My Awards-----------------
+ 
+
+  
+ 
+    
+    const rows = [
+      {
+        key: "1",
+        name: "Top Learner",
+        when: "10.10.23",
+      },
+      {
+        key: "2",
+        name: "Top Challenger",
+        when: "11.11.23",
+      },
+      {
+        key: "3",
+        name: "JS Master",
+        when: "20.11.23",
+      },
+      {
+        key: "4",
+        name: "Solid Vaper",
+        when: "12.12.23",
+
+      },
+    ];
+    
+    const columns = [
+      {
+        key: "name",
+        label: "What",
+      },
+      {
+        key: "when",
+        label: "When",
+      },
+
+    ];
+    
+    const myAwards = (
+        <Table isCompact  removeWrapper radius="none" shadow="none" isStriped layout="auto" className="pb-4">
+          <TableHeader columns={columns}>
+            {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+          </TableHeader>
+          <TableBody items={rows}>
+            {(item) => (
+              <TableRow data-hover key={item.key}>
+                {(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      );
+
+//-----------------My Awards-----------------
+
+  const leaderBoard = (
+    <ul className="pb-2">
+      <li className="flex items-center justify-between pb-2">
+        <User
+          className=""
+          name="Jane Doe"
+          description="1842 pts"
+          avatarProps={{
+            src: "https://i.pravatar.cc/150?u=a04258114e29026702d",
+          }}
+        />
+        <h3 className="text-lg font-extralight">#1</h3>
+      </li>
+      <li className="flex items-center justify-between pb-2">
+        <User
+          className=""
+          name="Pablo Bennett"
+          description="1501 pts"
+          avatarProps={{
+            src: "https://i.pravatar.cc/150?img=15",
+          }}
+        />
+        <h3 className="text-lg font-extralight">#2</h3>
+      </li>
+      <li className="flex items-center justify-between">
+        <User
+          className=""
+          name="Elsa Shaw"
+          description="501 pts"
+          avatarProps={{
+            src: "https://i.pravatar.cc/150?img=9",
+          }}
+        />
+        <h3 className="text-lg font-extralight">#3</h3>
+      </li>
+    </ul>
   );
+
 
   return (
     <Accordion variant="splitted" selectionMode="multiple" isCompact>
@@ -32,12 +116,11 @@ export default function LeftPanel() {
         key="1"
         aria-label="My Awards"
         title="My Awards"
-        classNames={{ content: "list-disc" }}
       >
         {myAwards}
       </AccordionItem>
       <AccordionItem key="2" aria-label="Leader board" title="Leader Board">
-        {defaultContent}
+        {leaderBoard}
       </AccordionItem>
     </Accordion>
   );
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,32 +2,33 @@ import { button as buttonStyles } from "@nextui-org/theme";
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 import TremorShell from "./components/charts/TremorMetrics";
 import LeftPanel from "./components/ui/leftPanel";
+import RightPanel from "./components/ui/rightPanel";
 
 export default function Dashboard() {
   return (
     // <div className="absolute z-20 top-20 inset-x-0 flex justify-center overflow-hidden pointer-events-none">
     <div className="container flex mx-auto max-w-7xl px-6 h-screen">
       <div id="leftPanel" className=" basis-1/5  ">
-        <h2 className="text-xl font-medium p-4">Achievements</h2>
+        <h2 className="text-xl text-center font-medium p-4">Achievements</h2>
         <LeftPanel />
       </div>
       <div id="centerPanel" className=" flex-grow ">
         <TremorShell />
       </div>
       <div id="rightPanel" className="flex-col basis-1/5 ">
-        <div className="flex items-center justify-center ">
-          <div>Dashboard</div>
-          <LogoutLink
-            className={buttonStyles({
-              color: "danger",
-              radius: "full",
-              variant: "ghost",
-              size: "sm",
-            })}
-          >
-            Sign out
-          </LogoutLink>
-        </div>
+        {/* <LogoutLink
+              className={buttonStyles({
+                color: "danger",
+                radius: "full",
+                variant: "ghost",
+                size: "sm",
+              })}
+            >
+              Sign out
+            </LogoutLink> */}
+        <h2 className="text-xl text-center font-medium p-4">Widgets</h2>
+
+        <RightPanel />
       </div>
     </div>
   );
