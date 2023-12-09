545cac1ad7379ec75ae5a94b65175c8215ee59fa
m-a-mohsen <ma.abdel.mohsen@gmail.com>
2023-12-09 15:37:04 +0100

    user navbar

---
+++ app/dashboard/components/ui/usernavbar.tsx
@@ -0,0 +1,117 @@
+'use client'
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { useRouter } from "next/navigation";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Input,
+  DropdownItem,
+  DropdownTrigger,
+  Dropdown,
+  DropdownMenu,
+    Avatar,
+    Kbd,
+} from "@nextui-org/react";
+
+import { SearchIcon } from "../../../../components/icons";
+
+export default function UserNavbar() {
+
+    	const searchInput = (
+        <Input
+          aria-label="Search"
+          classNames={{
+            inputWrapper: "bg-default-100",
+            input: "text-sm",
+            base: "max-w-full sm:max-w-[10rem] h-10",
+          }}
+          endContent={
+            <Kbd className="hidden lg:inline-block" keys={["command"]}>
+              K
+            </Kbd>
+          }
+          labelPlacement="outside"
+          placeholder="Search..."
+          startContent={
+            <SearchIcon className="pointer-events-none flex-shrink-0 text-base text-default-400" />
+          }
+          type="search"
+        />
+      );
+  return (
+    <Navbar className="mb-2 rounded-2xl" isBlurred={true}>
+      <NavbarContent justify="start">
+        <NavbarContent className="hidden gap-3 sm:flex">
+          <NavbarItem isActive>
+            <Link
+              className=" text-xl"
+              color="primary"
+              href="/dashboard"
+            >
+              Dashboard
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href="#" aria-current="page" color="foreground">
+              Sessions
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="/">
+              Spaced repetitions
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+      </NavbarContent>
+
+      <NavbarContent as="div" className="items-center" justify="end">
+        <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
+        {/* <Input
+          classNames={{
+            base: "max-w-full sm:max-w-[10rem] h-10",
+            mainWrapper: "h-full",
+            input: "text-small",
+            inputWrapper:
+              "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+          }}
+          placeholder="Type to search..."
+          size="sm"
+          startContent={<SearchIcon size={18} />}
+          type="search"
+        /> */}
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://avatars.githubusercontent.com/u/52429360?v=4&s=150"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <span className=" ">Signed in as</span>
+              <span className="font-light"> (User)</span>
+              <p className="font-semibold">ma.abdel.mohsen@gmail.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            {/* <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem> */}
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger" href="/api/auth/logout">
+              Sign out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
+    </Navbar>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,16 +7,18 @@ import LearningPoints from "./components/charts/learningpoints";
 import EngagementPoints from "./components/charts/engagementpoints";
 import MotivationPoints from "./components/charts/motivationpoints";
 import PointsProgress from "./components/charts/pointsprogress";
+import UserNavbar from "./components/ui/usernavbar";
 
 export default function Dashboard() {
   return (
     // <div className="absolute z-20 top-20 inset-x-0 flex justify-center overflow-hidden pointer-events-none">
-    <div className="container mx-auto flex max-w-7xl px-6 border-2">
+    <div className="container mx-auto flex max-w-7xl px-6 ">
       <div id="leftPanel" className=" basis-1/5  ">
-        <h2 className="p-4 text-center text-xl font-medium">Achievements</h2>
+        <h2 className="mt-1 p-4 text-center">Achievements</h2>
         <LeftPanel />
       </div>
       <div id="centerPanel" className="flex-grow">
+        <UserNavbar />
         {/* <TremorShell /> */}
         <div className="grid gap-2 px-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
           <LearningPoints />
@@ -24,23 +26,12 @@ export default function Dashboard() {
           <MotivationPoints />
         </div>
         <div className="my-10 px-4">
-
-        <PointsProgress />
+          <PointsProgress />
         </div>
       </div>
       <div id="rightPanel" className="basis-1/5 flex-col ">
-        <h2 className="p-4 text-center text-xl font-medium">Widgets</h2>
+        <h2 className="p-4 text-center ">Widgets</h2>
         <RightPanel />
-        {/* <LogoutLink
-              className={buttonStyles({
-                color: "danger",
-                radius: "full",
-                variant: "ghost",
-                size: "sm",
-              })}
-            >
-              Sign out
-            </LogoutLink> */}
       </div>
     </div>
   );
